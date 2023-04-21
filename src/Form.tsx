a28c08900fc718d6bc46a4d5c75fc2f6ac62c960
Dhruv Patel <dhruv225@outlook.com>
2023-04-21 01:48:09 -0400

    create useStates and fix input errors

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,34 +1,48 @@
+import { useState } from "react";
+
 type FormProps = {
     firstName: string;
     lastName: string;
     email: string;
     phone: string;
+    
 }
 
 export const Form = (props: FormProps) => {
+    const [firstName, setFirstName] = useState(props.firstName);
+    const [lastName, setLastName] = useState(props.lastName);
+    const [email, setEmail] = useState(props.email);
+    const [phone, setPhone] = useState(props.phone);
+
+    
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+    }
+
+   
+
+
     return (
-            <>
+            <form onSubmit={handleSubmit} className="form">
                 <label>
                     First Name:
-                    <input type="text" value={props.firstName}></input>
+                    <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)}></input>
                 </label>
                 
                 <label>
                     Last Name:
-                    <input type="text" value={props.lastName}></input>
+                    <input type="text" value={lastName} onChange={e => setLastName(e.target.value)}></input>
                 </label>
                 <label>
                     Email:
-                    <input type="email" value={props.email}></input>
+                    <input type="email" value={email} onChange={e => setEmail(e.target.value)}></input>
                 </label>
                 <label>
                     Email:
-                    <input type="tel" value={props.phone}></input>
+                    <input type="tel" value={phone} onChange={e => setPhone(e.target.value)}></input>
                 </label>
-
-                <button>Submit</button>
-            </>
-                
-
+                    <button type="submit">Submit</button>
+           </form>
+         
         )
 }
